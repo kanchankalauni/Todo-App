d162d9071df5559248160ddc86ef0e2a19d41eb0
Kanchan Kalauni <kanchankalauni03@gmail.com>
2024-05-06 12:45:12 +0530

    update

---
+++ index.js
@@ -0,0 +1,38 @@
+let addBtn = document.getElementById('addBtn')
+let i = 1
+
+addBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    let todoInput = document.getElementById('todoInput')
+    // console.log(todoInput)
+    if (todoInput.value == '') {
+        alert("Enter any todo first !")
+        return
+    } 
+    else {
+        let div = document.createElement('div')
+        div.setAttribute('class', 'singleTodo')
+        console.log(div)
+        div.innerHTML = `
+            <h2>${todoInput.value}</h2>
+            <input type="checkbox" name="" id="">
+            <button id="remBtn${i}">Remove Todo</button>
+        `
+        document.getElementById('allTodo').appendChild(div)
+        todoInput.value = ''
+        document.getElementById(`remBtn${i}`).addEventListener('click', (e) => {
+            console.log(e.target)
+            console.log(e.target.parentNode)
+            e.target.parentNode.remove()
+            // console.log("first")
+            // document.getElementById('singleTodo').remove(div)
+            // document.getElementById('allTodo').removeChild(div)
+        })
+        i++
+    }
+    
+})
+
+// document.getElementById('remBtn').addEventListener('click', () => {
+//     document.getElementById('allTodo').removeChild(div)
+// })
