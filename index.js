93d9541b7effaa74959cff1f796db213b72571f4
Kanchan Kalauni <kanchankalauni03@gmail.com>
2024-05-11 18:53:07 +0530

    update

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
 let addBtn = document.getElementById('addBtn')
 let i = 1
 
+let data = JSON.parse(localStorage.getItem('allTodo')) || []
+
+
+
+function displayTodo() {
+    document.getElementById('allTodo').innerHTML = ''
+    data.map((singleTodo) => {
+        console.log(singleTodo)
+        todoStructure(singleTodo)
+    })
+}
+
+displayTodo()
+
+function todoStructure(singleTodo) {
+    let div = document.createElement('div')
+    div.setAttribute('class', 'singleTodo')
+    console.log("div 1",div)
+    div.innerHTML = `
+        <h2>${singleTodo}</h2>
+        <input type="checkbox" name="" id="checkbox${i}" onclick=markTodo(${i})>
+        <button id="remBtn${i}" onclick=removeTodo(${i})>Remove Todo</button>
+    `
+    document.getElementById('allTodo').appendChild(div)
+    i++
+    // console.log("div 2",div)
+}
+
 addBtn.addEventListener('click', (e) => {
     e.preventDefault()
     let todoInput = document.getElementById('todoInput')
+    data.push(todoInput.value)
     // console.log(todoInput)
     if (todoInput.value == '') {
         alert("Enter any todo first !")
         return
     } 
     else {
-        let div = document.createElement('div')
-        div.setAttribute('class', 'singleTodo')
-        console.log(div)
-        div.innerHTML = `
-            <h2>${todoInput.value}</h2>
-            <input type="checkbox" name="" id="checkbox${i}" onclick=markTodo(${i})>
-            <button id="remBtn${i}" onclick=removeTodo(${i})>Remove Todo</button>
-        `
-        document.getElementById('allTodo').appendChild(div)
+        localStorage.setItem('allTodo',JSON.stringify(data))
+        displayTodo()
         todoInput.value = ''
 
         // document.getElementById(`remBtn${i}`).addEventListener('click', (e) => {
@@ -30,7 +52,7 @@ addBtn.addEventListener('click', (e) => {
         //     e.target.previousElementSibling.classList.toggle("strike")
         // })
 
-        i++
+        
     }
 })
 
